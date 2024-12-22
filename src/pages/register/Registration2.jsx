765266f551dd4ae940ac08359b4101fe7e29a845
Devesh Agarwal <deveshagarwal652005@gmail.com>
2024-12-23 00:52:34 +0530

    form routes connected

--- src/pages/register/Registration2.jsx
+++ src/pages/register/Registration2.jsx
@@ -6,42 +6,30 @@ import TeamLeaderForm from "./TeamLeaderForm";
 import Rules from "./Rules";
 import MemberForm from "./MemberForm";
 import { useParams } from "react-router-dom";
-import cocbg from "../../assets/illustration/coc-bg.jpg";
+import HomeBackground from "../../components/homeBackground";
+import { SPORTS } from "./Constants";
+import UnderConstruction from "../underConstruction"
 
 const Registration2 = () => {
-  const { sport } = useParams();
+  const { sport: sport_name } = useParams();
+  const [sport, setSport] = useState(null);
   const [college, setCollege] = useState("");
   const [category, setCategory] = useState("");
   const [teamName, setTeamName] = useState("");
   const [leaderDetails, setLeaderDetails] = useState({});
   const [memberDetails, setMemberDetails] = useState([]);
   const [step, setStep] = useState(1);
-  const [players, setPlayers] = useState(0);
-
-  const SPORTS = [
-    "Cricket",
-    "Football",
-    "Badminton_(Singles)",
-    "Badminton_(Doubles)",
-  ];
-  const PLAYERS = {
-    cricket: 11,
-    football: 15,
-    "badminton_(singles)": 1,
-    "badminton_(doubles)": 2,
-  };
 
   const scriptURL =
     "https://script.google.com/macros/s/AKfycbwxrQMHKa2hYdXgZrJ-EfKY7_sEDGNdt--aEB175uQHFNr7kLA1LxINyWAXP9ACwBqu/exec";
 
   useEffect(() => {
-    if (sport) {
-      setPlayers(PLAYERS[sport]);
-    }
+    const s = SPORTS.find((sport) => sport.name === sport_name) || null;
+    console.log(s);
+    setSport(s);
   }, []);
 
   useEffect(() => {
-    // Validation for enabling the "Next" button
     if (!college) setStep(1);
     else if (!category) setStep(2);
     else if (!teamName) setStep(3);
@@ -57,7 +45,7 @@ const Registration2 = () => {
 
   const handleSubmit = async () => {
     const payload = {
-      sport,
+      sport: sport.name,
       college,
       category,
       teamName,
@@ -93,73 +81,75 @@ const Registration2 = () => {
 
   return (
     <>
-      <img
-        src={cocbg}
-        alt=""
-        className="fixed inset-0 w-screen object-cover h-screen top-0 bottom-0 -z-10"
-      />
-      <div className="flex align-middle justify-center w-full">
-        <div className="min-h-screen w-4/5 flex justify-start items-start border backdrop-blur-[5px] border-gray-200 mt-[80px] z-10 rounded-2xl">
-          <div className="min-w-96">
-            <Rules sport={sport} />
-          </div>
-          <div className="overflow-y-auto flex-grow px-8 py-6 border-l">
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit();
-              }}
-            >
-              <CollegeForm value={college} setValue={setCollege} />
-              <OptionForm
-                index="2"
-                ques={"Select Category"}
-                value={category}
-                setValue={setCategory}
-                options={["Male", "Female"]}
-                enabled={step >= 2}
-              />
-              {college !== "NIT Raipur" && (
-                <InputForm
-                  value={teamName}
-                  setValue={setTeamName}
-                  ques={"Team Name?"}
-                  enabled={step >= 3}
-                />
-              )}
-              {college === "NIT Raipur" && (
-                <OptionForm
-                  index="4"
-                  ques={"Select Group"}
-                  value={teamName}
-                  setValue={setTeamName}
-                  options={["IT + Mech", "CSE + META"]}
-                  enabled={step >= 3}
-                />
-              )}
-              <TeamLeaderForm
-                leaderDetails={leaderDetails}
-                setLeaderDetails={setLeaderDetails}
-                enabled={step >= 4}
-              />
-              <MemberForm
-                value={memberDetails}
-                setValue={setMemberDetails}
-                count={players - 1}
-                enabled={step >= 5}
-              />
-              <div className="flex gap-3 mt-2">
-                <button
-                  className="focus:outline-none text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
-                  type="submit"
-                >
-                  Submit
-                </button>
+      {!sport ? (
+        <UnderConstruction />
+      ) : (
+        <>
+          <HomeBackground />
+          <div className="flex align-middle justify-center w-full">
+            <div className="w-4/5 flex justify-start items-start border backdrop-blur-[5px] border-gray-200 my-[80px] z-10 rounded-2xl">
+              <div className="min-w-96">
+                <Rules sport={sport} />
               </div>
-            </form>
+              <div className="overflow-y-auto flex-grow px-8 py-6 border-l">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                  }}
+                >
+                  <CollegeForm value={college} setValue={setCollege} />
+                  <OptionForm
+                    index="2"
+                    ques={"Select Category"}
+                    value={category}
+                    setValue={setCategory}
+                    options={["Male", "Female"]}
+                    enabled={step >= 2}
+                  />
+                  {college !== "NIT Raipur" && (
+                    <InputForm
+                      value={teamName}
+                      setValue={setTeamName}
+                      ques={"Team Name?"}
+                      enabled={step >= 3}
+                    />
+                  )}
+                  {college === "NIT Raipur" && (
+                    <OptionForm
+                      index="4"
+                      ques={"Select Group"}
+                      value={teamName}
+                      setValue={setTeamName}
+                      options={["IT + Mech", "CSE + META"]}
+                      enabled={step >= 3}
+                    />
+                  )}
+                  <TeamLeaderForm
+                    leaderDetails={leaderDetails}
+                    setLeaderDetails={setLeaderDetails}
+                    enabled={step >= 4}
+                  />
+                  <MemberForm
+                    value={memberDetails}
+                    setValue={setMemberDetails}
+                    count={sport.players - 1}
+                    enabled={step >= 5}
+                  />
+                  <div className="flex gap-3 mt-2">
+                    <button
+                      className="focus:outline-none text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,11 +1,44 @@
+import cocbg from "../../assets/illustration/coc-bg.jpg";
+import { SPORTS } from "./Constants";
+import HomeBackground from "../../components/homeBackground";
+import "./register.css";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
-	return (
-		<div className='w-full h-screen absolute bg-black text-white'>
-			<div className="mt-[80px]">
-				Register
-			</div>
-		</div>
-	)
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HomeBackground />
+      <div className="flex w-full align-middle justify-center pt-5">
+        <div className="w-[90%] py-10">
+          <div className="flex gap-10 flex-wrap justify-center">
+            {SPORTS.map((sport, index) => (
+              <div
+                className="sport-cont"
+                key={index}
+                onClick={(e) => {
+                  navigate(`/register/${sport.name}`);
+                }}
+              >
+                <img
+                  className="object-cover h-[200px] object-center w-full"
+                  src={sport.image}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-center text-gray-200 drop-shadow">
+                    {sport.display}
+                  </h3>
+                  <p className="text-center ">Players: {sport.players}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Register;
